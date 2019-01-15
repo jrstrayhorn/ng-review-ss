2e3efe6b901ec47d158f26b03463dd24274b4c3a
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-01-15 15:57:14 -0500

    adding cart detail/summary components, updating store component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { StoreComponent } from "./store/store.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+import { CheckoutComponent } from "./store/checkout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "store", component: StoreComponent },
+  { path: "cart", component: CartDetailComponent },
+  { path: "checkout", component: CheckoutComponent },
+  { path: "**", redirectTo: "/store" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
