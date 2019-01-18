38d304b148095fdf9f34dacbb0fa63d4cd5fa0b4
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-01-18 16:28:00 -0500

    done with admin order management

--- src/app/admin/orderTable.component.ts
+++ src/app/admin/orderTable.component.ts
@@ -1,8 +1,27 @@
 import { Component } from "@angular/core";
+import { OrderRepository } from "../model/order.repository";
+import { Order } from "../model/order.model";
 
 @Component({
-  template: `
-    <div class="bg-info p-2 text-white"><h3>Order Table Placeholder</h3></div>
-  `
+  templateUrl: "orderTable.component.html"
 })
-export class OrderTableComponent {}
+export class OrderTableComponent {
+  includeShipped = false;
+
+  constructor(private repository: OrderRepository) {}
+
+  getOrders(): Order[] {
+    return this.repository
+      .getOrders()
+      .filter(o => this.includeShipped || !o.shipped);
+  }
+
+  markShipped(order: Order) {
+    order.shipped = true;
+    this.repository.updateOrder(order);
+  }
+
+  delete(id: number) {
+    this.repository.deleteOrder(id);
+  }
+}
